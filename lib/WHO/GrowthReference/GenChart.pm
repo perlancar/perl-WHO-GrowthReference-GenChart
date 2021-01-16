6911f337ab8c85ec82d3c1161c6b8e4691a6a92a
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-16 21:18:31 +0700

    Adjust to WHO::GrowthReference::Table 0.007

--- lib/WHO/GrowthReference/GenChart.pm
+++ lib/WHO/GrowthReference/GenChart.pm
@@ -122,7 +122,7 @@ sub gen_who_growth_chart_from_table {
     my (@age,
         @height, @height_zm3, @height_zm2, @height_zm1, @height_z0, @height_z1, @height_z2, @height_z3,
         @weight, @weight_zm3, @weight_zm2, @weight_zm1, @weight_z0, @weight_z1, @weight_z2, @weight_z3,
-        @bmi   ,            , @bmi_zm2   , @bmi_zm1   , @bmi_z0   , @bmi_z1   , @bmi_z2   ,
+        @bmi   , @bmi_zm3   , @bmi_zm2   , @bmi_zm1   , @bmi_z0   , @bmi_z1   , @bmi_z2   , @bmi_z3,
     );
     my $i = -1;
   SET_DATA_SETS: {
@@ -139,31 +139,31 @@ sub gen_who_growth_chart_from_table {
             my $res = WHO::GrowthReference::Table::get_who_growth_reference(
                 gender => $gender,
                 defined($date_key) ? (dob => $dob, now => $time) : (age => 365.25*86400*$row->{$age_key}),
-                defined($height_key) ? (height => $row->{$height_key}) : (),
-                defined($weight_key) ? (weight => $row->{$weight_key}) : (),
+                defined($height_key) && length($row->{$height_key}) ? (height => $row->{$height_key}) : (),
+                defined($weight_key) && length($row->{$weight_key}) ? (weight => $row->{$weight_key}) : (),
             );
             return [400, "Table row[$i]: Cannot get WHO growth reference data: $res->[0] - $res->[1]"]
                 unless $res->[0] == 200;
             $res->[2]{age} =~ /^(\d+(?:\.\d+)?)/ or die; push @age, $1/12;
             if (defined $height_key) {
                 push @height, $row->{$height_key};
-                push @height_z0 , $res->[2]{height_Z0};
-                push @height_zm1, $res->[2]{'height_Z-1'};
-                push @height_z1 , $res->[2]{'height_Z+1'};
-                push @height_zm2, $res->[2]{'height_Z-2'};
-                push @height_z2 , $res->[2]{'height_Z+2'};
-                push @height_zm3, $res->[2]{'height_Z-3'};
-                push @height_z3 , $res->[2]{'height_Z+3'};
+                push @height_z0 , $res->[2]{height_SD0};
+                push @height_zm1, $res->[2]{'height_SD1neg'};
+                push @height_z1 , $res->[2]{'height_SD1'};
+                push @height_zm2, $res->[2]{'height_SD2neg'};
+                push @height_z2 , $res->[2]{'height_SD2'};
+                push @height_zm3, $res->[2]{'height_SD3neg'};
+                push @height_z3 , $res->[2]{'height_SD3'};
             }
             if (defined $weight_key) {
                 push @weight, $row->{$weight_key};
-                push @weight_z0 , $res->[2]{weight_Z0};
-                push @weight_zm1, $res->[2]{'weight_Z-1'};
-                push @weight_z1 , $res->[2]{'weight_Z+1'};
-                push @weight_zm2, $res->[2]{'weight_Z-2'};
-                push @weight_z2 , $res->[2]{'weight_Z+2'};
-                push @weight_zm3, $res->[2]{'weight_Z-3'};
-                push @weight_z3 , $res->[2]{'weight_Z+3'};
+                push @weight_z0 , $res->[2]{weight_SD0};
+                push @weight_zm1, $res->[2]{'weight_SD1neg'};
+                push @weight_z1 , $res->[2]{'weight_SD1'};
+                push @weight_zm2, $res->[2]{'weight_SD2neg'};
+                push @weight_z2 , $res->[2]{'weight_SD2'};
+                push @weight_zm3, $res->[2]{'weight_SD3neg'};
+                push @weight_z3 , $res->[2]{'weight_SD3'};
             }
             if (defined $height_key && defined $weight_key) {
                 if ($row->{$weight_key} && $row->{$height_key}) {
@@ -171,11 +171,13 @@ sub gen_who_growth_chart_from_table {
                 } else {
                     push @bmi, undef;
                 }
-                push @bmi_z0 , $res->[2]{bmi_Z0};
-                push @bmi_zm1, $res->[2]{'bmi_Z-1'};
-                push @bmi_z1 , $res->[2]{'bmi_Z+1'};
-                push @bmi_zm2, $res->[2]{'bmi_Z-2'};
-                push @bmi_z2 , $res->[2]{'bmi_Z+2'};
+                push @bmi_z0 , $res->[2]{bmi_SD0};
+                push @bmi_zm1, $res->[2]{'bmi_SD1neg'};
+                push @bmi_z1 , $res->[2]{'bmi_SD1'};
+                push @bmi_zm2, $res->[2]{'bmi_SD2neg'};
+                push @bmi_z2 , $res->[2]{'bmi_SD2'};
+                push @bmi_zm3, $res->[2]{'bmi_SD3neg'};
+                push @bmi_z3 , $res->[2]{'bmi_SD3'};
             }
         }
     } # SET_DATA_SETS
@@ -338,6 +340,20 @@ sub gen_who_growth_chart_from_table {
             ydata => \@bmi_zm2,
             title => 'bmi z-2',
             color => 'red',
+            style => 'lines',
+        );
+        push @datasets, Chart::Gnuplot::DataSet->new(
+            xdata => \@age,
+            ydata => \@bmi_z3,
+            title => 'bmi z+2',
+            color => 'black',
+            style => 'lines',
+        );
+        push @datasets, Chart::Gnuplot::DataSet->new(
+            xdata => \@age,
+            ydata => \@bmi_zm3,
+            title => 'bmi z-2',
+            color => 'black',
             style => 'lines',
         );
     }
